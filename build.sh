0ffe31de4988d20a8674bbe33cb63e0ad0f2fe38
atelierbram <bramdeh@gmail.com>
2023-01-29 14:49:33 +0100

    build critical css for remote

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@
 write_to_file()
 {
 
+cp "source/assets/css/critical.min.css" "source/_partials/critical-css.blade.php"
 cp "source/assets/css/home.min.css" "source/_partials/home-css.blade.php"
 cp "source/assets/js/theme-switch.min.js" "source/_partials/theme-switch-js.blade.php"
 
